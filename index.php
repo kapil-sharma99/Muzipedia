cdab805610bcb5313965ecec58469d01f7d7c5c9
kapil-sharma99 <ks2822123@gmail.com>
2021-10-14 14:08:12 +0530

    Footer half done

--- index.php
+++ index.php
@@ -16,9 +16,58 @@ if(isset($_SESSION['userLoggedIn'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Muzipedia</title>
+  <title>Welcome to Muzipedia</title>
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-  Hello
+  <div id="nowPlayingBarContainer">
+		<div id="nowPlayingBar">
+			<div id="nowPlayingLeft">
+
+			</div>
+			<div id="nowPlayingCenter">
+				<div class="content playerControls">
+					<div class="buttons">
+						<button class="controlButton shuffle" title="Shuffle Button">
+							<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+						</button>
+
+						<button class="controlButton previous" title="Previous Button">
+							<img src="assets/images/icons/previous.png" alt="Previous">
+						</button>
+
+						<button class="controlButton play" title="Play Button">
+							<img src="assets/images/icons/play.png" alt="Play">
+						</button>
+
+						<button class="controlButton pause" title="Pause Button" style="display: none">
+							<img src="assets/images/icons/pause.png" alt="Pause">
+						</button>
+
+						<button class="controlButton next" title="Next Button">
+							<img src="assets/images/icons/next.png" alt="Next">
+						</button>
+
+						<button class="controlButton repeat" title="Repeat Button">
+							<img src="assets/images/icons/repeat.png" alt="Repeat">
+						</button>
+					</div>
+
+					<div class="playbackBar">
+						<span class="progressTime current">0.00</span>
+						<div class="progressBar">
+							<div class="progressBarBg">
+								<div class="progress"></div>
+							</div>
+						</div>
+						<span class="progressTime remaining">0.00</span>
+					</div>
+				</div>
+			</div>
+			<div id="nowPlayingRight">
+
+			</div>
+		</div>
+	</div>
 </body>
 </html>
