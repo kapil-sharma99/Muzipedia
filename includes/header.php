edc2e1b584dd3a6c513c2d58c8954b8522700be9
kapil-sharma99 <ks2822123@gmail.com>
2021-10-14 18:49:02 +0530

    Structure updated

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<?php 
+
+include("includes/config.php");
+//session_destroy(); used for logout
+if(isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+	header("Location: register.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to Muzipedia</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <div id="mainContainer">
+		<div id="topContainer">
+			<?php include("includes/navBarContainer.php"); ?>
+			<div id="mainViewContainer">
+				<div id="mainContent">
