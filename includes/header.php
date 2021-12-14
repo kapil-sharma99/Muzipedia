74fab5ca6e9ae6610c1dbaa719741a4a2818f473
kapil-sharma99 <ks2822123@gmail.com>
2021-12-14 17:25:04 +0530

    Seamless transitions section started

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@ include("includes/classes/Song.php");
 //session_destroy(); used for logout
 if(isset($_SESSION['userLoggedIn'])) {
 	$userLoggedIn = $_SESSION['userLoggedIn'];
+	echo "<script>userLoggedIn = '$userLoggedIn';</script>";
 } else {
 	header("Location: register.php");
 }
