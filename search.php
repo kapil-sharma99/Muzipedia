832e9af51e5caf8aa5e25c879672b9e1270c7685
kapil-sharma99 <ks2822123@gmail.com>
2021-12-20 19:42:54 +0530

    Auto Page Reload

--- search.php
+++ search.php
@@ -12,5 +12,20 @@ if(isset($_GET['term'])){
 
 <div class="searchContainer">
   <h4>Search For an Artist, album or Song</h4>
-  <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start Typing...">
+  <input type="text" class="searchInput" value="<?php echo $term; ?>" onfocus="this.value = this.value" placeholder="Start Typing...">
 </div>
+
+<script>
+  $(".searchInput").focus();
+  $(function() {
+    var timer;
+    $(".searchInput").keyup(function() {
+      clearTimeout(timer);
+
+      timer = setTimeout(function() {
+        var val = $(".searchInput").val();
+        openPage("search.php?term="+val);
+      }, 2000);
+    })
+  })
+</script>
