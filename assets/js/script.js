291852fc51c63b57c6ed4ff92a62094f37763742
kapil-sharma99 <ks2822123@gmail.com>
2022-01-17 19:42:48 +0530

    Menu hide done

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,17 @@ var shuffle = false;
 var userLoggedIn;
 var timer;
 
+$(document).click(function(click) {
+  var target = $(click.target);
+  if(!target.hasClass("item") && !target.hasClass("optionsButton")) {
+    hideOptionsMenu();
+  }
+});
+
+$(window).scroll(function() {
+  hideOptionsMenu();
+})
+
 function openPage(url) {
   if(timer != null) {
     clearTimeout(timer);
@@ -62,6 +73,12 @@ function showOptionsMenu(button) {
   menu.css({"top": top + "px", "left": left-menuWidht + "px", "display": "inline"});
 }
 
+function hideOptionsMenu() {
+  var menu = $(".optionsMenu");
+  if(menu.css("display") != "none") {
+    menu.css("display", "none");
+  }
+}
 
 function formatTime(seconds) {
   var time = Math.round(seconds);
